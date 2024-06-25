ac2e68530d1614522ca151a3efb3ea966dfdb04e
raghav161 <raghavbansal242003@gmail.com>
2024-06-25 13:55:35 +0530

    added login backend

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require('bcryptjs'); // Assuming bcrypt is used for hashing passwords
 
 module.exports.getAllUsers = async (req, res) => {
     try {
@@ -17,11 +18,14 @@ module.exports.createUser = async (req, res) => {
         // Check if the email already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-        return res.status(400).json({ error: 'Email already exists' });
+            return res.status(409).json({ message: 'Email already exists' });
         }
     
+        // Hash the password before saving to the database
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // Create a new user instance
-        const newUser = new User({ name, email, password, contactNumber, gender, dob });
+        const newUser = new User({ name, email, password: hashedPassword, contactNumber, gender, dob });
     
         // Save the user to the database
         await newUser.save();
@@ -29,6 +33,29 @@ module.exports.createUser = async (req, res) => {
         res.status(201).json({ message: 'User created successfully' });
     } catch (err) {
         console.error('Error creating user', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(403).json({ message: "User doesn't exists" });
+        }
+
+        // Check if the password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful', user });
+    } catch (error) {
+        console.error('Error logging in', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/user')
 router.get("/getUsers", userController.getAllUsers)
 
 router.post("/createUser", userController.createUser)
-
+router.post('/login', userController.loginUser);
 
 module.exports = router
